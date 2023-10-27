8d2e4ca99512a1e66b86bfcf224ec7750c924c28
rizalgithub7755 <147589425+rizalgithub7755@users.noreply.github.com>
2023-10-27 21:03:45 +0700

    Add files via upload

---
+++ poliklinik/pasien.php
@@ -0,0 +1,185 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      Sistem Informasi Poliklinik
+    </a>
+    <button class="navbar-toggler"
+    type="button" data-bs-toggle="collapse"
+    data-bs-target="#navbarNavDropdown"
+    aria-controls="navbarNavDropdown" aria-expanded="false"
+    aria-label="Toggle navigation">
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="index.php">
+            Home
+          </a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button"
+          data-bs-toggle="dropdown" aria-expanded="false">
+            Data Master
+          </a>
+          <ul class="dropdown-menu">
+            <li>
+              <a class="dropdown-item" href="dokter.php?page=dokter">
+                Dokter
+              </a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="pasien.php?page=pasien">
+                Pasien
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" 
+          href="periksa.php?page=periksa">
+            Periksa
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<?php
+include'koneksi.php';
+?>
+<body>
+    <div class="container">
+    <form class="form row" method="POST" action="" name="myForm" onsubmit="return(validate());">
+    <!-- Kode php untuk menghubungkan form dengan database -->
+    <?php
+    $nama = '';
+    $alamat = '';
+    $no_hp = '';
+    if (isset($_GET['id'])) {
+        $ambil = mysqli_query($mysqli, 
+        "SELECT * FROM pasien 
+        WHERE id='" . $_GET['id'] . "'");
+        while ($row = mysqli_fetch_array($ambil)) {
+            $nama = $row['nama'];
+            $alamat = $row['alamat'];
+            $no_hp = $row['no_hp'];
+        }
+    ?>
+        <input type="hidden" name="id" value="<?php echo
+        $_GET['id'] ?>">
+    <?php
+    }
+    ?>
+     <div class="col">
+        <label for="inputNama" class="form-label fw-bold">
+            Nama Pasien
+        </label>
+        <input type="varchar" class="form-control" name="nama" id="inputNama" placeholder="Nama Pasien" value="<?php echo $nama ?>">
+    </div>
+    <div class="col">
+        <label for="inputAlamat" class="form-label fw-bold">
+            Alamat
+        </label>
+        <input type="varchar" class="form-control" name="alamat" id="inputAlamat" placeholder="Alamat" value="<?php echo $alamat ?>">
+    </div>
+    <div class="col mb-2">
+        <label for="inputNo_hp" class="form-label fw-bold">
+        Nomor Handphone
+        </label>
+        <input type="varchar" class="form-control" name="no_hp" id="inputNo_hp" placeholder="Nomor Handphone" value="<?php echo $no_hp ?>">
+    </div>
+    <div class="col">
+        <button type="submit" class="btn btn-primary rounded-pill px-3" name="simpan">Simpan</button>
+    </div>
+    <!-- Table-->
+<table class="table table-hover">
+    <!--thead atau baris judul-->
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nama Pasien</th>
+            <th scope="col">Alamat</th>
+            <th scope="col">Nomor Handphone</th>
+            <th scope="col">Aksi</th>
+        </tr>
+    </thead>
+    <!--tbody berisi isi tabel sesuai dengan judul atau head-->
+    <tbody>
+    <?php
+   $result = mysqli_query(
+    $mysqli,"SELECT * FROM pasien ORDER BY nama"
+    );
+$no = 1;
+while ($data = mysqli_fetch_array($result)) {
+?>
+    <tr>
+        <td><?php echo $no++ ?></td>
+        <td><?php echo $data['nama'] ?></td>
+        <td><?php echo $data['alamat'] ?></td>
+        <td><?php echo $data['no_hp'] ?></td>
+                <td>
+                    <a class="btn btn-info rounded-pill px-3" 
+                    href="pasien.php?id=<?php echo $data['id'] ?>">Ubah
+                    </a>
+                    <a class="btn btn-danger rounded-pill px-3" 
+                    href="pasien.php?id=<?php echo $data['id'] ?>&aksi=hapus">Hapus
+                    </a>
+                </td>
+            </tr>
+        <?php
+        }
+        ?>
+
+    </tbody>
+</form>
+<?php
+if (isset($_POST['simpan'])) {
+    if (isset($_POST['id'])) {
+        $ubah = mysqli_query($mysqli, "UPDATE Pasien SET 
+                                        nama = '" . $_POST['nama'] . "',
+                                        alamat = '" . $_POST['alamat'] . "',
+                                        no_hp = '" . $_POST['no_hp'] . "'
+                                        WHERE
+                                        id = '" . $_POST['id'] . "'");
+    } else {
+        $tambah = mysqli_query($mysqli, "INSERT INTO pasien (nama, alamat, no_hp) 
+        VALUES (
+            '" . $_POST['nama'] . "',
+            '" . $_POST['alamat'] . "',
+            '" . $_POST['no_hp'] . "'
+                                            )");
+    }
+
+    echo "<script> 
+            document.location='pasien.php';
+            </script>";
+}
+
+if (isset($_GET['aksi'])) {
+    if ($_GET['aksi'] == 'hapus') {
+        $hapus = mysqli_query($mysqli, "DELETE FROM pasien WHERE id = '" . $_GET['id'] . "'");
+    } else if ($_GET['aksi'] == 'ubah_status') {
+        $ubah_status = mysqli_query($mysqli, "UPDATE pasien SET 
+                                        status = '" . $_GET['status'] . "' 
+                                        WHERE
+                                        id = '" . $_GET['id'] . "'");
+    }
+
+    echo "<script> 
+            document.location='pasien.php';
+            </script>";
+}
+?>
+</main>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
